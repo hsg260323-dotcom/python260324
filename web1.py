d21d8d7252aceb0b80c772b1dcfeb4597254e616
hong <hsg260323@gmail.com>
2026-03-25 16:44:58 +0900

    웹크롤링2

--- web1.py
+++ web1.py
@@ -10,9 +10,34 @@ soup = BeautifulSoup(page, "html.parser")
 
 # #<p>태그의 텍스트만 추출하기
 # print(soup.p.string)
-print(soup.find_all("p"))
+# print(soup.find_all("p"))
+# print(soup.find("p"))
+# print(soup.find_all("p", class_="outer-text"))
+# print(soup.find_all("p", attrs={"class": "outer-text"}))
+# print(soup.find_all(id="first"))
+for tag in soup.find_all("p"):
+    title = tag.text.strip()
+    title = title.replace("\n", "")
+    print(title)
 
 
+#문자열처리 메서드와 정규표현식
+strA = "<<< python >>>"
+result = strA.strip("<> ")
+print(result)
+strB = result.replace("python", "python javascript")
+print(strB)
+result = "spam egg spam egg".split()
+print(result)
+print(":".join(result))
 
+#정규표현식: 특정한 패턴(규칙)문자열
+import re
+
+result = re.search(r"\d{4}", "올해는 2026년입니다.")
+print(result.group())
+
+result = re.search("apple", "this is apple")
+print(result.group())
 
 
